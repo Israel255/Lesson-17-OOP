25bbd0393e57b86ca340fc1301a941cddf37622e
Israel255 <62959042+Israel255@users.noreply.github.com>
2020-07-17 16:05:58 +0300

    Add files via upload

---
+++ Lesson _17_OOP/Citizen.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson__17_OOP
+{
+    class Citizen
+    {
+        public string _name;
+        public Citizen[] _children;
+        public readonly int _father_id;
+        public readonly int _id;
+        public static int _number_of_current_citizens = 30;
+        public const int _MAXIMUM_NUMBER_OF_CITIZENS = 100;
+
+        public Citizen(string name, int id)
+        {
+            _name = name;
+            _id = id;
+        }
+        public Citizen(string name, int father_id, int id)
+        {
+            _name = name;
+            _father_id = father_id;
+            _id = id;
+        }
+
+        public static void Print_Number_Of_Citizens()
+        {
+            Console.WriteLine("Number of citizens: " + _number_of_current_citizens);
+        }
+
+        public static bool Reached_Half_Of_Maximum_Size()
+        {
+            if (_number_of_current_citizens <= _MAXIMUM_NUMBER_OF_CITIZENS)
+            {
+               return false;
+            }
+            else
+            {
+               return true;
+            }
+        }
+        public void Print_Id()
+        {
+            Console.WriteLine("The id is: " + _id);
+        }
+        public void Print_Gap_Between_My_Id_And_Father()
+        {
+            if (_father_id >= _id)
+            {
+                Console.WriteLine("The difference is: " + (_father_id - _id));
+            }
+            else
+            {
+                Console.WriteLine("The difference is: " + (_id - _father_id));
+            }
+        }
+        public void Set_Children(Citizen[] children)
+        {
+            _children = children;
+        }
+
+        public override string ToString()
+        {
+            return "Citizen name: " + _name + " Number of children: " + _children.Length + " Id number: " + _father_id + " Number of citizens: " + _number_of_current_citizens;
+        }
+    }
+}
---
+++ Lesson _17_OOP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson__17_OOP
+{
+    class Program
+    {
+        static bool Has_Children(Citizen c)
+        {
+            if (c._children.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool Check_Validity(Citizen c)
+        {
+            if (Has_Children(c) == false)
+            {
+                return false;
+            }
+            else
+            {
+                int my_child = 0;
+                for (int i = 0; i < c._children.Length; i++)
+                {
+                    if (c._id == c._children[i]._father_id)
+                    {
+                        my_child++;
+                    }
+                }
+                if (my_child == c._children.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Citizen piter = new Citizen("Piter", 13579);
+            Citizen mario = new Citizen("Mario", 13579, 24680);
+            Citizen alan = new Citizen("Alan", 13579,57834);
+            Citizen martin = new Citizen("Martin", 13571,45322);
+            Citizen[] children = { mario, alan, martin };
+            piter.Set_Children(children);
+            Console.WriteLine(piter);
+            Console.WriteLine(Has_Children(piter));
+            Console.WriteLine(Check_Validity(piter));
+            Citizen.Print_Number_Of_Citizens();
+            Console.WriteLine(Citizen.Reached_Half_Of_Maximum_Size());
+            piter.Print_Id();
+            mario.Print_Gap_Between_My_Id_And_Father();
+        }
+    }
+}
